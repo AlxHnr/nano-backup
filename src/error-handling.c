1a1c2a020d412c73270c405277220db07a751b6a
Alexander Heinrich <alxhnr@nudelpost.de>
2015-06-12 19:52:25 +0200

    Implemented simple functions for error handling

---
+++ src/error-handling.c
@@ -0,0 +1,74 @@
+/*
+  Copyright (c) 2015 Alexander Heinrich <alxhnr@nudelpost.de>
+
+  This software is provided 'as-is', without any express or implied
+  warranty. In no event will the authors be held liable for any damages
+  arising from the use of this software.
+
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+
+     1. The origin of this software must not be misrepresented; you must
+        not claim that you wrote the original software. If you use this
+        software in a product, an acknowledgment in the product
+        documentation would be appreciated but is not required.
+
+     2. Altered source versions must be plainly marked as such, and must
+        not be misrepresented as being the original software.
+
+     3. This notice may not be removed or altered from any source
+        distribution.
+*/
+
+/**
+  @file error-handling.c Implements various functions for error handling.
+*/
+
+#include "error-handling.h"
+
+#include <stdio.h>
+#include <errno.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
+
+/** Prints an error message and terminates the program. It takes the same
+  arguments as printf().
+
+  @param format A valid formating string. This string doesn't need to
+  contain newlines.
+  @param ... Additional arguments.
+*/
+void die(const char *format, ...)
+{
+  va_list arguments;
+  va_start(arguments, format);
+
+  fprintf(stderr, "error: ");
+  vfprintf(stderr, format, arguments);
+  fprintf(stderr, "\n");
+
+  va_end(arguments);
+
+  exit(EXIT_FAILURE);
+}
+
+/** Almost identical to die(), but it also prints a description of the
+  current errno value.
+
+  @see die()
+*/
+void dieErrno(const char *format, ...)
+{
+  va_list arguments;
+  va_start(arguments, format);
+
+  fprintf(stderr, "error: ");
+  vfprintf(stderr, format, arguments);
+  fprintf(stderr, ": %s\n", strerror(errno));
+
+  va_end(arguments);
+
+  exit(EXIT_FAILURE);
+}
---
+++ src/error-handling.h
@@ -0,0 +1,42 @@
+/*
+  Copyright (c) 2015 Alexander Heinrich <alxhnr@nudelpost.de>
+
+  This software is provided 'as-is', without any express or implied
+  warranty. In no event will the authors be held liable for any damages
+  arising from the use of this software.
+
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+
+     1. The origin of this software must not be misrepresented; you must
+        not claim that you wrote the original software. If you use this
+        software in a product, an acknowledgment in the product
+        documentation would be appreciated but is not required.
+
+     2. Altered source versions must be plainly marked as such, and must
+        not be misrepresented as being the original software.
+
+     3. This notice may not be removed or altered from any source
+        distribution.
+*/
+
+/**
+  @file error-handling.h Declares various functions for error handling.
+*/
+
+#ifndef _NANO_BACKUP_ERROR_HANDLING_H_
+#define _NANO_BACKUP_ERROR_HANDLING_H_
+
+#ifdef __GNUC__
+#define CHECK_FORMAT __attribute__((format(printf, 1, 2)))
+#else
+#define CHECK_FORMAT
+#endif
+
+extern void die(const char *format, ...) CHECK_FORMAT;
+extern void dieErrno(const char *format, ...) CHECK_FORMAT;
+
+#undef CHECK_FORMAT
+
+#endif
