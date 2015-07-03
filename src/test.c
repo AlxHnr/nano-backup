ea39701738133ea3ed0596b7e0a8ebbe740fc130
Alexander Heinrich <alxhnr@nudelpost.de>
2015-07-03 14:43:51 +0200

    Implemented functions to for unit testing

---
+++ src/test.c
@@ -0,0 +1,211 @@
+/*
+  Copyright (c) 2015 Alexander Heinrich <alxhnr@nudelpost.de>
+
+  This software is provided 'as-is', without any express or implied
+  warranty. In no event will the authors be held liable for any damages
+  arising from the use of this software.
+
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+
+     1. The origin of this software must not be misrepresented; you must
+        not claim that you wrote the original software. If you use this
+        software in a product, an acknowledgment in the product
+        documentation would be appreciated but is not required.
+
+     2. Altered source versions must be plainly marked as such, and must
+        not be misrepresented as being the original software.
+
+     3. This notice may not be removed or altered from any source
+        distribution.
+*/
+
+/**
+  @file test.c Implements various functions required for testing. This
+  module conflicts with error-handling.c, since it implements the same
+  functions in a different way.
+*/
+
+#include "test.h"
+
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+#include <stdint.h>
+#include <limits.h>
+
+/** A global jump buffer. It should not be used directly. */
+jmp_buf test_jump_buffer;
+
+/** Contains the error message of the last die() invocation or NULL. This
+  variable should not be used directly. */
+char *test_error_message = NULL;
+
+/** True, if a call to die() or dieErrno() should be handled with a longjmp
+  back into the last assert statement. This variable should not be used
+  directly. */
+bool expecting_error = false;
+
+/** Frees the memory in test_error_message. */
+static void freeTestErrorMessage(void)
+{
+  free(test_error_message);
+}
+
+/** Assigns allocated memory to test_error_message and uses it to store the
+  formated error message. If this function fails to do so, it will
+  terminate the test suite with an error message.
+
+  @param format A format string.
+  @param arguments An initialised va_list containing all arguments
+  specified in the format string.
+  @param length The length of the formated message without the terminating
+  null byte.
+  @param buffer_length The length of the buffer that should be allocated.
+  Must be at least the size of the given length plus 1.
+*/
+static void populateTestErrorMessage(const char *format, va_list arguments,
+                                     int length, size_t buffer_length)
+{
+  /* Register cleanup function on the first call of this function. */
+  if(test_error_message == NULL) atexit(freeTestErrorMessage);
+
+  /* Free the previous memory stored in test_error_message. */
+  free(test_error_message);
+
+  test_error_message = malloc(buffer_length);
+  if(test_error_message == NULL)
+  {
+    dieTest("failed to allocate space to store the error message");
+  }
+
+  int bytes_copied = vsprintf(test_error_message, format, arguments);
+  if(bytes_copied != length)
+  {
+    dieTest("failed to copy the error message");
+  }
+}
+
+/* The next two functions are alternative implementations of the die() and
+   dieErrno() functions. If expecting_error is true, they will store the
+   error message in test_error_message and jump back into the last assert
+   statement. Otherwise they will terminate the program with failure.
+*/
+
+void die(const char *format, ...)
+{
+  va_list arguments;
+
+  va_start(arguments, format);
+  int format_length = vsnprintf(NULL, 0, format, arguments);
+  va_end(arguments);
+
+  if(format_length < 0 || format_length == INT_MAX)
+  {
+    dieTest("failed to calculate the error message length");
+  }
+
+  va_start(arguments, format);
+  populateTestErrorMessage(format, arguments, format_length,
+                           format_length + 1);
+  va_end(arguments);
+
+  if(expecting_error)
+  {
+    longjmp(test_jump_buffer, 1);
+  }
+  else
+  {
+    dieTest("%s", test_error_message);
+  }
+}
+
+void dieErrno(const char *format, ...)
+{
+  va_list arguments;
+
+  va_start(arguments, format);
+  int format_length = vsnprintf(NULL, 0, format, arguments);
+  va_end(arguments);
+
+  size_t errno_string_length = strlen(strerror(errno));
+
+  /* Ensure that the formated message, ": ", errnos translated string and
+     the terminating null byte fit into the string. */
+  if(format_length < 0 ||
+     errno_string_length > SIZE_MAX - format_length - 3)
+  {
+    dieTest("failed to calculate the error message length");
+  }
+
+  va_start(arguments, format);
+  populateTestErrorMessage(format, arguments, format_length,
+                           format_length + errno_string_length + 3);
+  va_end(arguments);
+
+  test_error_message[format_length] = ':';
+  test_error_message[format_length + 1] = ' ';
+  strcpy(&test_error_message[format_length + 2], strerror(errno));
+
+  if(expecting_error)
+  {
+    longjmp(test_jump_buffer, 1);
+  }
+  else
+  {
+    dieTest("%s", test_error_message);
+  }
+}
+
+/** Prints a fancy error message and terminates the test suite. It takes
+  the same arguments as printf(). This function should not be called
+  directly.
+
+  @param format A valid formating string. This string doesn't need to
+  contain newlines.
+  @param ... Additional arguments.
+*/
+void dieTest(const char *format, ...)
+{
+  va_list arguments;
+  va_start(arguments, format);
+
+  fprintf(stderr, "[FAILURE]\n    ");
+
+  if(!expecting_error) fprintf(stderr, "unexpected error: ");
+
+  vfprintf(stderr, format, arguments);
+  fprintf(stderr, "\n");
+
+  va_end(arguments);
+
+  exit(EXIT_FAILURE);
+}
+
+/** Prints a fancy message which indicates that a test group was entered.
+  This function must be called before any use of assert_true or
+  assert_error, otherwise it will lead to messed up output on the terminal.
+
+  @param name The name of the test group.
+*/
+void testGroupStart(const char *name)
+{
+  fprintf(stderr, "  Testing %s", name);
+
+  for(size_t index = strlen(name); index < 61; index++)
+  {
+    fputc('.', stderr);
+  }
+}
+
+/** Prints a fancy success message. This must be called before another test
+  group starts or the program exits. Otherwise it will lead to messed up
+  output on the terminal.
+*/
+void testGroupEnd(void)
+{
+  fprintf(stderr, "[success]\n");
+}
---
+++ src/test.h
@@ -0,0 +1,72 @@
+/*
+  Copyright (c) 2015 Alexander Heinrich <alxhnr@nudelpost.de>
+
+  This software is provided 'as-is', without any express or implied
+  warranty. In no event will the authors be held liable for any damages
+  arising from the use of this software.
+
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+
+     1. The origin of this software must not be misrepresented; you must
+        not claim that you wrote the original software. If you use this
+        software in a product, an acknowledgment in the product
+        documentation would be appreciated but is not required.
+
+     2. Altered source versions must be plainly marked as such, and must
+        not be misrepresented as being the original software.
+
+     3. This notice may not be removed or altered from any source
+        distribution.
+*/
+
+/**
+  @file test.h Declares functions for testing code. This header should only
+  be included by tests.
+*/
+
+#ifndef _NANO_BACKUP_TEST_H_
+#define _NANO_BACKUP_TEST_H_
+
+#include <string.h>
+#include <setjmp.h>
+#include <stdbool.h>
+
+/** Asserts that the given expression evaluates to true. It will catch
+  calls to die(). */
+#define assert_true(expression) \
+  if(setjmp(test_jump_buffer) == 0) { \
+    if(!(expression)) { \
+      dieTest("%s: line %i: assert failed: %s", \
+              __FILE__, __LINE__, #expression); \
+    } \
+  } else { \
+    dieTest("%s: line %i: fatal error: %s", __FILE__, \
+            __LINE__, test_error_message); \
+  }
+
+/** Asserts that the given expression causes a call to die() with the
+  specified error message. */
+#define assert_error(expression, message) \
+  expecting_error = true; \
+  if(setjmp(test_jump_buffer) == 0) { \
+    (void)(expression); \
+    dieTest("%s: line %i: expected error: %s", \
+            __FILE__, __LINE__, #expression); \
+  } else if(strcmp(message, test_error_message) != 0) { \
+    dieTest("%s: line %i: got wrong error message: \"%s\"\n" \
+            "\t\texpected: \"%s\"", __FILE__, __LINE__, \
+            test_error_message, message); \
+  } expecting_error = false;
+
+extern jmp_buf test_jump_buffer;
+extern char *test_error_message;
+extern bool expecting_error;
+
+extern void dieTest(const char *format, ...);
+
+extern void testGroupStart(const char *name);
+extern void testGroupEnd(void);
+
+#endif
