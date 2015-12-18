abebd9a14e71a79f2b0495dc2031b6669b167684
Alexander Heinrich <alxhnr@nudelpost.de>
2015-12-18 11:52:28 +0100

    Generalized fallback init script

--- test/full program tests/fallback targets/init.sh
+++ test/full program tests/fallback targets/init.sh
@@ -1,6 +1,44 @@
+# Various helper functions.
+
+# Generates "generated/expected-output".
+#
+# $1 The full or relative path to the template file.
+# $2 The path which should be prepended to every found file in the output.
+gen_expected_output()
+{
+  path_elements=$(echo "$2" | grep -o '\/' | wc -l)
+  expected_files=$(grep -E '^Total: \+[0-9]+ items' "$1" | \
+    sed -r 's,^Total: \+([0-9]+) items.*$,\1,g')
+  total_files=$(($expected_files + $path_elements))
+
+  sed -r "s,^(.. )\/(.*)$,\1$2/\2,g" "$1" | \
+    sed -r "s,^Total: \+$expected_files items,Total: +$total_files items,g" | \
+    sort > generated/expected-output
+}
+
+# Generates "generated/repo/config".
+#
+# $1 The full or relative path to the template file.
+# $2 The path which should be prepended to every path in the config file.
+gen_config_file()
+{
+  mkdir -p generated/repo
+  sed -r "s,^\/,$2/,g" "$1" > generated/repo/config
+}
+
+# Initialize the test.
 mkdir -p generated
 
+# Generate expected output.
 if test -f expected-output; then
-  sed -r "s,^(.. )\/(.*)$,\1${PWD}/\2,g" expected-output | \
-    sort > generated/expected-output
+  gen_expected_output "expected-output" "$PWD"
+elif test -f expected-output-test-data; then
+  gen_expected_output "expected-output-test-data" "$PROJECT_PATH/test/data"
+fi
+
+# Generate config file.
+if test -f config; then
+  gen_config_file "config" "$PWD"
+elif test -f config-test-data; then
+  gen_config_file "config-test-data" "$PROJECT_PATH/test/data"
 fi
