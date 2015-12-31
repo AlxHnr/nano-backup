1a604e137a2e8db1f5c19b807de223b13ebc4dd3
Alexander Heinrich <alxhnr@nudelpost.de>
2015-12-31 13:27:06 +0100

    Added test for all mismatched path warnings

---
+++ test/full program tests/mismatched path warnings/all warnings/post-init.sh
@@ -0,0 +1,16 @@
+mkdir -p generated/foo/abc/dir/
+touch generated/foo/abc/dir/test.txt
+
+mkdir -p generated/foo/xyz/
+touch generated/foo/xyz/dir
+
+mkdir -p generated/a/b/c/d/e/f/g/h/i/j/k/l/m/n/o/p/q/r/s/t/u/v/w/x/y/z/1/2/3/4/5/6/7/8/9/0/
+echo "This is a dummy string." > generated/a/b/c/d/e/f/g/dummy.txt
+
+mkdir -p generated/FOO/
+touch generated/FOO/BAR.txt
+touch generated/FOO/test.txt
+
+mkdir -p generated/BAR/bird
+echo "Another string." > generated/BAR/bird/file.txt
+touch generated/BAR/gentoo
