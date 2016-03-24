9cf371774ef47644861d514a24a5cc35b8a722bd
Alexander Heinrich <alxhnr@nudelpost.de>
2016-03-24 14:40:00 +0100

    Check generated filenames inside repositories

--- test/full program tests/fallback targets/run.sh
+++ test/full program tests/fallback targets/run.sh
@@ -12,3 +12,20 @@ if test -f expected-repo-files; then
   ls -A generated/repo/ | sort > generated/repo-files
   diff -q generated/repo-files generated/expected-repo-files
 fi
+
+# Check generated filenames inside the repository.
+for file in generated/repo/*-*-*; do
+  test -e "$file" || continue
+
+  hash=${file#*-}
+  hash=${hash%-*}
+  real_hash=$(sha1sum "$file")
+  real_hash=${real_hash%% *}
+  test "$hash" = "$real_hash" ||
+    (echo "invalid hash in filename: \"$file\"" && false)
+
+  size=${file##*-}
+  real_size=$(stat --format="%s" "$file")
+  test "$size" = "$real_size" ||
+    (echo "invalid size in filename: \"$file\"" && false)
+done
