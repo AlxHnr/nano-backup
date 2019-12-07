553e4684a771388bef6b790fb694a1d50774bc88
Alexander Heinrich <alxhnr@users.noreply.github.com>
2019-12-07 20:10:33 +0100

    Move regex-pool to CRegion

--- src/regex-pool.c
+++ src/regex-pool.c
@@ -7,24 +7,15 @@
 
 #include <stdlib.h>
 
-#include "memory-pool.h"
-#include "safe-wrappers.h"
+#include "CRegion/global-region.h"
+#include "CRegion/region.h"
+
 #include "error-handling.h"
 
-static regex_t **regex_pool = NULL;
-static size_t regex_pool_used = 0;
-static size_t regex_pool_capacity = 0;
-
-/** Frees the entire regex pool. */
-static void freeRegexPool(void)
+/** @param data Pointer to a regex_t object to be released. */
+static void releaseRegex(void *data)
 {
-  for(size_t index = 0; index < regex_pool_used; index++)
-  {
-    regfree(regex_pool[index]);
-    free(regex_pool[index]);
-  }
-
-  free(regex_pool);
+  regfree(data);
 }
 
 /** Compiles the given regular expression and terminates the program on
@@ -41,36 +32,19 @@ const regex_t *rpCompile(const char *expression,
                          const char *file_name,
                          size_t line_nr)
 {
-  /* Grow regex pool if its used up. */
-  if(regex_pool_used == regex_pool_capacity)
-  {
-    if(regex_pool == NULL) sAtexit(freeRegexPool);
-
-    size_t new_pool_length =
-      regex_pool_capacity == 0 ? 4 : sSizeMul(regex_pool_capacity, 2);
-    size_t new_pool_size = sSizeMul(sizeof *regex_pool, new_pool_length);
-
-    regex_pool = sRealloc(regex_pool, new_pool_size);
-    regex_pool_capacity = new_pool_length;
-  }
-
-  regex_t *regex = sMalloc(sizeof *regex);
-  int error = regcomp(regex, expression, REG_EXTENDED | REG_NOSUB);
+  regex_t *regex = CR_RegionAlloc(CR_GetGlobalRegion(), sizeof(*regex));
+  const int error = regcomp(regex, expression, REG_EXTENDED | REG_NOSUB);
 
   if(error != 0)
   {
-    size_t error_length = regerror(error, regex, NULL, 0);
-    char *error_str = mpAlloc(error_length);
+    const size_t error_length = regerror(error, regex, NULL, 0);
+    char *error_str = CR_RegionAlloc(CR_GetGlobalRegion(), error_length);
     regerror(error, regex, error_str, error_length);
-    free(regex);
-
     die("%s: line %zu: %s: \"%s\"", file_name,
         line_nr, error_str, expression);
   }
 
-  /* Store reference in regex pool. */
-  regex_pool[regex_pool_used] = regex;
-  regex_pool_used++;
+  CR_RegionAttach(CR_GetGlobalRegion(), releaseRegex, regex);
 
   return regex;
 }
