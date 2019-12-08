e65ee319d4768f590f5268a6e90c37164ec0cf19
Alexander Heinrich <alxhnr@users.noreply.github.com>
2019-12-08 23:00:23 +0100

    Remove .ycm_extra_conf.py

--- .ycm_extra_conf.py
+++
@@ -1,8 +0,0 @@
-def FlagsForFile(filename, **kwargs):
-  return {
-    'flags': [
-      '-std=c99', '-D_XOPEN_SOURCE=600', '-D_FILE_OFFSET_BITS=64',
-      '-Wall', '-Wextra', '-Isrc/'
-      ],
-    'do_cache': True
-  }
